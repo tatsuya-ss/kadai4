c7385ad50b91e65ad676fc657e3c5786fd3826b7
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-12-07 23:12:51 +0900

    private追加。viewDidLoad削除。

--- kadai4/ViewController.swift
+++ kadai4/ViewController.swift
@@ -9,23 +9,16 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet private var label: UILabel!
-    var labelNumber = 0
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
+    private var labelNumber = 0
 
     @IBAction func plusButton(_ sender: Any) {
         labelNumber += 1
         label.text = String(labelNumber)
     }
     
-    
     @IBAction func clearButton(_ sender: Any) {
         labelNumber = 0
         label.text = String(labelNumber)
     }
-    
 }
 
